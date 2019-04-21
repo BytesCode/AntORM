69535bfad740b2f13e6db318dd83833dd78d008b
BytesCode <18864122500@163.com>
2019-04-21 18:33:15 +0800

    IDataReader转换成List

---
+++ AntORM/DataTableSerialize.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+namespace AntORM
+{
+    public class DataTableSerialize
+    {
+        /// <summary>
+        /// DataTable 转换List
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static List<T> DatatableList<T>(DataTable table)
+        {
+            if (table == null || table.Columns.Count == 0)
+                return new List<T>();
+
+            List<T> list = new List<T>();
+            Type type = typeof(T);
+
+            foreach (DataRow item in table.Rows)
+            {
+                object obj = Activator.CreateInstance(type);
+                foreach (var prop in type.GetProperties())
+                    prop.SetValue(obj, item[prop.Name], null);
+
+                list.Add((T)obj);
+            }
+            return list;
+        }
+
+
+        /// <summary>
+        /// 将IDataReader 转换成List
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public static List<T> DataReaderToListEntity<T>(IDataReader reader) where T : new()
+        {
+            List<T> list = new List<T>();
+            if (reader != null)
+            {
+                while (reader.Read())
+                    list.Add(DataReaderToEntity<T>(reader));
+            }
+            return list;
+        }
+
+
+        /// <summary>
+        /// 将 DataReader转换成指定 实体
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public static T DataReaderToEntity<T>(IDataReader reader) where T : new()
+        {
+            T t = new T();
+            //获取需要映射实体的 所有属性
+            PropertyInfo[] propertys = t.GetType().GetProperties();
+
+            //存储IDataReader 读取出来的所有列名
+            List<string> FieldNameList = new List<string>();
+
+            //循环reader 将 列名加入list
+            for (int i = 0; i < reader.FieldCount; i++)
+                FieldNameList.Add(reader.GetName(i));
+
+            //循环需要映射的属性
+            foreach (PropertyInfo property in propertys)
+            {
+                //判断 该值 是否可写入
+                if (!property.CanWrite)
+                    continue;
+
+                //获取字段名
+                string FieldName = property.Name;
+                if (FieldNameList.Contains(FieldName))
+                {
+                    object value = reader[FieldName];
+                    if (value is DBNull)
+                        continue;
+
+                    try
+                    {
+                        property.SetValue(t, value, null);
+                    }
+                    catch { continue; }
+                }
+            }
+            return t;
+        }
+    }
+}
--- AntORM/SqlManager.cs
+++ AntORM/SqlManager.cs
@@ -1,9 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace AntORM
 {
-    public class SqlManager
+    public class SqlManage
     {
+        private static readonly string conStr = "Data Source=.;Initial Catalog=studyCode;Integrated Security=True";
 
+        /// <summary>
+        /// 执行SQL， 返回受影响行数
+        /// </summary>
+        /// <param name="sql">Sql文本</param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static int ExecuteNonQuery(string sql, params SqlParameter[] param)
+        {
+
+            using (SqlConnection conn=new SqlConnection(conStr))
+            {
+                using (SqlCommand cmd=new SqlCommand(sql,conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    if (param != null)
+                    {
+                        cmd.Parameters.AddRange(param);
+                    }
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 执行SQL，获取List对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sqlTxt"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static List<T> ExecuteListEntity<T>(string sqlTxt, params SqlParameter[] param) where T : new()
+        {
+            using (SqlConnection conn = new SqlConnection(conStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sqlTxt, conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    if (param != null)
+                    {
+                        cmd.Parameters.AddRange(param);
+                    }
+                    conn.Open();
+                    return DataTableSerialize.DataReaderToListEntity<T>(cmd.ExecuteReader());
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行SQL，获取单个对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sqlTxt"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static T ExecuteEntity<T>(string sqlTxt, params SqlParameter[] param) where T : new()
+        {
+            using (SqlConnection conn = new SqlConnection(conStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sqlTxt, conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    if (param != null)
+                    {
+                        cmd.Parameters.AddRange(param);
+                    }
+                    conn.Open();
+                    return DataTableSerialize.DataReaderToEntity<T>(cmd.ExecuteReader());
+                }
+            }
+        }
     }
 }
